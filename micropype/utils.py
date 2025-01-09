c92b08f2b7ccd23720464866847df64b792f1dfb
Bastien Cagna <bastien.cagna@icm-institute.org>
2025-01-09 12:37:27 +0100

    fix check_dir for file paths

--- micropype/utils.py
+++ micropype/utils.py
@@ -7,11 +7,13 @@ from datetime import datetime
 
 def check_dir(*args, dir_exist_ok=True) -> str:
     path = op.abspath(op.join(*args))
-    if op.isfile(path):
+
+    # if path is file path like
+    if "." in op.split(path)[1]:
         path, _ = op.split(path)
-        # raise IOError(f'{path} is a file instead of a directory.')
+
     if not dir_exist_ok and op.isdir(path):
-        raise IOError(f'{path} already exist.')
+        raise IOError(f"{path} already exist.")
     makedirs(path, exist_ok=True)
     return path
 
@@ -19,29 +21,37 @@ def check_dir(*args, dir_exist_ok=True) -> str:
 def datetime_str():
     return datetime.isoformat(datetime.now())[:-4]
 
-class MessageIntent(Enum):
-    DEFAULT=0
-    INFO=1
-    SUCCESS=2
-    ERROR=3
-    WARNING=4
-    HIGHLIGHT=5
 
-def cprint(*args, intent:MessageIntent=MessageIntent.DEFAULT):
-    """ Print a (optionally colored) message with the date and time"""
-    if intent == MessageIntent.INFO: clr = Fore.BLUE
-    elif intent == MessageIntent.SUCCESS: clr = Fore.GREEN
-    elif intent == MessageIntent.ERROR: clr = Fore.RED
-    elif intent == MessageIntent.WARNING: clr = Fore.YELLOW
-    elif intent == MessageIntent.HIGHLIGHT: clr = Fore.LIGHTMAGENTA_EX
-    else: clr = Fore.WHITE
-    print(f'{datetime_str()}:', clr, " ".join(str(a) for a in args), Style.RESET_ALL)
+class MessageIntent(Enum):
+    DEFAULT = 0
+    INFO = 1
+    SUCCESS = 2
+    ERROR = 3
+    WARNING = 4
+    HIGHLIGHT = 5
+
+
+def cprint(*args, intent: MessageIntent = MessageIntent.DEFAULT):
+    """Print a (optionally colored) message with the date and time"""
+    if intent == MessageIntent.INFO:
+        clr = Fore.BLUE
+    elif intent == MessageIntent.SUCCESS:
+        clr = Fore.GREEN
+    elif intent == MessageIntent.ERROR:
+        clr = Fore.RED
+    elif intent == MessageIntent.WARNING:
+        clr = Fore.YELLOW
+    elif intent == MessageIntent.HIGHLIGHT:
+        clr = Fore.LIGHTMAGENTA_EX
+    else:
+        clr = Fore.WHITE
+    print(f"{datetime_str()}:", clr, " ".join(str(a) for a in args), Style.RESET_ALL)
 
 
 def merge_dicts(a: dict, b: dict) -> dict:
-    """ Override a with b values 
+    """Override a with b values
 
-        If a keys dot not exists in b, it is created
+    If a keys dot not exists in b, it is created
     """
     for key in b:
         if key in a and isinstance(a[key], dict) and isinstance(b[key], dict):
@@ -51,9 +61,8 @@ def merge_dicts(a: dict, b: dict) -> dict:
     return a
 
 
-
 def auto_cast(value: str):
-    """ Convert string to float, int, list or dict is possible """
+    """Convert string to float, int, list or dict is possible"""
     # # Try float
     # try:
     #     return float(value)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="micropype",
-    version="0.3.2",
+    version="0.3.3",
     packages=find_packages(),
     author="Bastien Cagna",
     description="Very basic pipelining toolbox",
